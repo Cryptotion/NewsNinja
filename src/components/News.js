edf62bd208953695149e3aba20981e6a4b97b44f
Krishna Pratap Kushwah <kushwahkrishna755@gmail.com>
2021-11-09 09:58:35 +0530

    done

--- src/components/News.js
+++ src/components/News.js
@@ -63,7 +63,7 @@ const News = (props) => {
         <h1 className="text-center my-3" style={{paddingTop:"50px"}}>NewsNinja - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
         {loading && <Spinner />}
         <InfiniteScroll
-          dataLength={articles.length}
+          // dataLength={articles.length}
           next={fetchMoreData}
           hasMore={articles.length !== totalResults}
           loader={<Spinner/>}
